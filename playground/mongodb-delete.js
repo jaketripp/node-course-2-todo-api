66f243e42f0e04726b5d1331d2c5c1458a7981ec
Jake Tripp <shmogens@gmail.com>
2017-10-13 16:20:07 -0500

    add delete elements section for mongo

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoAppp', (err, db) => {
+	if (err){
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+
+	// deleteMany
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+	// deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+	// findOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// db.collection('Users').deleteMany({name: 'Jake'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// db.collection('Users').findOneAndDelete({_id: new ObjectId("59e101f5806ded34ccb0b83a")}).then((result) => {
+	// 	console.log(result);
+	// });
+	// db.close();
+});
