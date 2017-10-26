38c17811a214ac4e03ef1299eb50913e5df19529
Jake Tripp <shmogens@gmail.com>
2017-10-25 20:30:26 -0500

    try to resolve issue with mondodb authentication

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,3 +4,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+// mongodb://jake:jake@ds235775.mlab.com:35775/node-todo-api
+
+// heroku config:set PROD_MONGODB=mongodb://jake:jake@ds235775.mlab.com:35775/node-todo-api
+
