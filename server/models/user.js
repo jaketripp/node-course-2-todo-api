6de0bd5bda45d3906b0ca8fb2c37051649c897f4
Jake Tripp <shmogens@gmail.com>
2017-11-18 18:31:58 -0600

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -57,6 +57,16 @@ UserSchema.methods.generateAuthToken = function(){
 	});
 };
 
+UserSchema.methods.removeToken = function(token){
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+};
+
 // model method
 // i.e. User model
 UserSchema.statics.findByToken = function(token){
